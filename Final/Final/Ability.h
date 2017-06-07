9a5f00704dd063855569ff651751961c9c78a16e
JenDrk <jendrk@ymail.com>
2017-06-07 01:54:46 -0700

    Added more to classes

--- Final/Final/Ability.h
+++ Final/Final/Ability.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <string>
 class Ability
 {
+private:
+	std::string _name;
 public:
 	Ability();
 	~Ability();
--- Final/Final/Hero.h
+++ Final/Final/Hero.h
@@ -5,8 +5,12 @@ class Hero
 private:
 	std::string _name;
 	int _health = 100;
+	int _level = 1;
+	int _xp;
 public:
 	Hero();
 	~Hero();
+
+	std::string virtual Attack() { return _name + "attacks!"; }
 };
 
